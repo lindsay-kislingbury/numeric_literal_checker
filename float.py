45b076e46ef29087016403a87b97ed3e53b50cd7
kailamanangan16 <kaila041603@icloud.com>
2025-04-11 15:34:41 -0700

    finished float nfa

--- float.py
+++ float.py
@@ -9,30 +9,43 @@ def create_float_nfa():
     states = {"dec_q0","dec_q1","dec_q2a","dec_q2b","dec_q3","dec_q4","dec_q5","dec_q6","dec_q7","dec_q8","dec_q9"}
 
     # Define alphabet
-    alphabet = set("0123456789_.+-")
+    alphabet = set("0123456789_.+-e")
 
     # Define transitions
     transitions = {}
 
-    # starts with 0
-    transitions[("dec_q0","0")] = {"dec_q1"} # if it starts with 0 
-    transitions[("dec_q1","0")] = {"dec_q1"} # if more 0s!
-    transitions[("dec_q1","_")] = {"dec_q2"} # if an _ is put in between
-    transitions[("dec_q2","0")] = {"dec_q1"} # if 0, brings it back to accepting
+    transitions[("dec_q0", ".")] = {"dec_q2b"}              #q0 -> q2b
 
-    # does not start with 0
-    for i in "123456789": # if it starts with a digit other than 0
-        transitions[("dec_q0", i)] = {"dec_q3"}
-    
-    transitions[("dec_q3","_")] = {"dec_q4"} # if _
+    for i in "0123456789":                                  # for any transitions with 0-9
+        transitions[("dec_q0", i)] = {"dec_q1"}             #q0 -> q1
+        transitions[("dec_q1", i)] = {"dec_q1"}             #q1 -> q1
+        transitions[("dec_q2a", i)] = {"dec_q3"}            #q2a -> q3
+        transitions[("dec_q2b", i)] = {"dec_q3"}            #q2a -> q3
+        transitions[("dec_q3", i)] = {"dec_q3"}             #q3 -> q23
+        transitions[("dec_q4", i)] = {"dec_q6"}             #q4 -> q5
+        transitions[("dec_q5", i)] = {"dec_q6"}             #q5 -> q6
+        transitions[("dec_q6", i)] = {"dec_q6"}             #q6 -> q6
+        transitions[("dec_q7", i)] = {"dec_q1"}             #q7 -> q1
+        transitions[("dec_q8", i)] = {"dec_q3"}             #q8 -> q3
+        transitions[("dec_q9", i)] = {"dec_q6"}             #q9 -> q6
 
-    for i in "0123456789": # if a digit is placed after _
-        transitions[("dec_q3", i)] = {"dec_q3"}
-        transitions[("dec_q4", i)] = {"dec_q3"}
+    transitions[("dec_q1", "_")] = {"dec_q7"}               #q1 -> q7
+    transitions[("dec_q1", "e")] = {"dec_q4"}               #q1 -> q4
+    transitions[("dec_q1", "E")] = {"dec_q4"}               #q1 -> q4
+    transitions[("dec_q1", ".")] = {"dec_q2a"}              #q1 -> q2a
 
+    transitions[("dec_q2a", "e")] = {"dec_q4"}              #q1 -> q2a
+    transitions[("dec_q2a", "E")] = {"dec_q4"}              #q1 -> q2a
+
+    transitions[("dec_q3", "_")] = {"dec_q8"}               #q3 -> q8
+
+    transitions[("dec_q4", "+")] = {"dec_q5"}               #q4 -> q5
+    transitions[("dec_q4", "-")] = {"dec_q5"}               #q4 -> q5
+
+    transitions[("dec_q6", "_")] = {"dec_q9"}               #q6 -> q9
 
     # Define accepting states
-    accepting_states = {"dec_q1", "dec_q3"}
+    accepting_states = {"dec_q2a", "dec_q3", "dec_q6"}
 
     # Create and return the NFA
     return NFA(states, alphabet, transitions, "dec_q0", accepting_states)
