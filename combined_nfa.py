2e50dba2c6f0ba3204a42b31a91b28ac8b91f137
Lindsay <lindsaymoses@gmail.com>
2025-04-11 19:01:11 -0700

    fix float transitions

--- combined_nfa.py
+++ combined_nfa.py
@@ -2,6 +2,7 @@ from nfa import NFA
 from decimal_nfa import create_decimal_nfa
 from octal_nfa import create_octal_nfa
 from hex_nfa import create_hex_nfa
+from float_nfa import create_float_nfa
 
 
 def create_combined_nfa():
@@ -12,12 +13,23 @@ def create_combined_nfa():
     dec_nfa = create_decimal_nfa()
     oct_nfa = create_octal_nfa()
     hex_nfa = create_hex_nfa()
+    float_nfa = create_float_nfa()
 
     # Create new combined NFA
-    states = {"start"}.union(dec_nfa.states).union(oct_nfa.states).union(hex_nfa.states)
+    states = (
+        {"start"}
+        .union(dec_nfa.states)
+        .union(oct_nfa.states)
+        .union(hex_nfa.states)
+        .union(float_nfa.states)
+    )
 
     # Combined alphabet
-    alphabet = dec_nfa.alphabet.union(oct_nfa.alphabet).union(hex_nfa.alphabet)
+    alphabet = (
+        dec_nfa.alphabet.union(oct_nfa.alphabet)
+        .union(hex_nfa.alphabet)
+        .union(float_nfa.alphabet)
+    )
 
     # Combined transitions
     transitions = {}
@@ -27,6 +39,7 @@ def create_combined_nfa():
         dec_nfa.initial_state,
         oct_nfa.initial_state,
         hex_nfa.initial_state,
+        float_nfa.initial_state,
     }
 
     # Add all transitions from individual NFAs
@@ -39,9 +52,14 @@ def create_combined_nfa():
     for (state, symbol), next_states in hex_nfa.transitions.items():
         transitions[(state, symbol)] = next_states
 
+    for (state, symbol), next_states in float_nfa.transitions.items():
+        transitions[(state, symbol)] = next_states
+
     # Combined accepting states
-    accepting_states = dec_nfa.accepting_states.union(oct_nfa.accepting_states).union(
-        hex_nfa.accepting_states
+    accepting_states = (
+        dec_nfa.accepting_states.union(oct_nfa.accepting_states)
+        .union(hex_nfa.accepting_states)
+        .union(float_nfa.accepting_states)
     )
 
     # Create and return the combined NFA
